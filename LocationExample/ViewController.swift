b7e9f803acc78e0529e8ffef9bcfe6a327a76251
Fatih Gumus <fatihgumus72@gmail.com>
2024-03-10 18:03:08 +0300

    Initial Commit

---
+++ LocationExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationExample
+//
+//  Created by Fatih Gümüş on 10.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
