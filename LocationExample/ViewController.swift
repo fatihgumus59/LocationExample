b656c689c5a070b3cc3405008b2efb92d93b5895
Fatih Gumus <fatihgumus72@gmail.com>
2024-03-10 19:14:42 +0300

    map kit -> live location tracking

--- LocationExample/ViewController.swift
+++ LocationExample/ViewController.swift
@@ -30,16 +30,6 @@ class ViewController: UIViewController {
         
         //41.03700175,28.974792
         
-        let konum = CLLocationCoordinate2D(latitude: 41.03700175, longitude: 28.974792)
-        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)//yakınlık dereces,
-        let bolge = MKCoordinateRegion(center: konum, span: span)
-        locationView.setRegion(bolge, animated: true)
-        
-        let pin = MKPointAnnotation()
-        pin.coordinate = konum
-        pin.title = "Şuanda Buradasınız"
-        pin.subtitle = "Son Konum"
-        locationView.addAnnotation(pin)
     }
 
 }
@@ -50,10 +40,29 @@ extension ViewController : CLLocationManagerDelegate {
         
         let sonKonum = locations[locations.count-1]// dizi içindeki son konum değerini bu şekilde alıyoruz.
         
+        let latitude = sonKonum.coordinate.latitude
+        let longitude = sonKonum.coordinate.longitude
         
-        labelEnlem.text = "ENLEM : \(sonKonum.coordinate.latitude)"
-        labelBoylam.text = "BOYLAM : \(sonKonum.coordinate.longitude)"
+        
+        labelEnlem.text = "ENLEM : \(latitude)"
+        labelBoylam.text = "BOYLAM : \(longitude)"
         labelHiz.text = "HIZ : \(sonKonum.speed)"
+        
+        let konum = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)//yakınlık dereces,
+        let bolge = MKCoordinateRegion(center: konum, span: span)
+        locationView.setRegion(bolge, animated: true)
+        
+        
+//        canlı konumu pin ekleyerek gösterir
+        
+//        let pin = MKPointAnnotation()
+//        pin.coordinate = konum
+//        pin.title = "\(latitude) - \(longitude)"
+//        pin.subtitle = "Şuanda Buradasınız"
+//        locationView.addAnnotation(pin)
+        
+        locationView.showsUserLocation = true // konumu canlı hareket ederken mavi şekilde gösteren kod
         
         
     }
