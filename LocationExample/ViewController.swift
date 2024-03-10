1c26b4c50d27a80037451d7e7eb9899d0a8f6e03
Fatih Gumus <fatihgumus72@gmail.com>
2024-03-10 19:05:48 +0300

    mapkit added

--- LocationExample/ViewController.swift
+++ LocationExample/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelEnlem: UILabel!
+    @IBOutlet weak var labelBoylam: UILabel!
+    @IBOutlet weak var labelHiz: UILabel!
+    
+    @IBOutlet weak var locationView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // en iyi konum değerini vermesi için
+        locationManager.requestWhenInUseAuthorization() // lokasyon izni için
+        locationManager.startUpdatingLocation()// lokasyon değerinin güncellenmesi için
+        locationManager.delegate = self
+        
+        //41.03700175,28.974792
+        
+        let konum = CLLocationCoordinate2D(latitude: 41.03700175, longitude: 28.974792)
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)//yakınlık dereces,
+        let bolge = MKCoordinateRegion(center: konum, span: span)
+        locationView.setRegion(bolge, animated: true)
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = konum
+        pin.title = "Şuanda Buradasınız"
+        pin.subtitle = "Son Konum"
+        locationView.addAnnotation(pin)
     }
 
-
 }
-
+extension ViewController : CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // güncel olarak konum değerlerini bize verecek.
+        
+        let sonKonum = locations[locations.count-1]// dizi içindeki son konum değerini bu şekilde alıyoruz.
+        
+        
+        labelEnlem.text = "ENLEM : \(sonKonum.coordinate.latitude)"
+        labelBoylam.text = "BOYLAM : \(sonKonum.coordinate.longitude)"
+        labelHiz.text = "HIZ : \(sonKonum.speed)"
+        
+        
+    }
+    
+}
